347c735577949428c83d73358d4b559ca86a919e
Bhaskar <bhaskarkotakonda123@gmail.com>
2025-03-12 19:59:22 +0530

    import

---
+++ React Task folder/import/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Import from "./components/import";
+
+
+
+function App(){
+  return(
+    <>
+    <Import/>
+    </>
+  );
+ 
+}
+
+export default App
---
+++ React Task folder/import/src/Components/Import.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Import(){
+    return(
+        <div>
+            <ul>
+                <li>front end</li>
+            </ul>
+        </div>
+    )
+}
+export default Import
