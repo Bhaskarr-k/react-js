080c54d3b6b3e0efeb42639aececb2e9323aa332
Bhaskar <bhaskarkotakonda123@gmail.com>
2025-03-18 22:55:13 +0530

    class components

---
+++ React programs/Classcomponent/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Header } from "./Header";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Footer } from "./Footer";
+
+const App = () => {
+  return(
+    <>
+    <Header/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
